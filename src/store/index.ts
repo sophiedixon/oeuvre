3d86fbef43811823b3131545584505b2debc8056
sophiedixon <dixonsophie@hotmail.com>
2017-02-16 15:59:02 +0000

    updates

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,102 +5,44 @@ export default {
 	"graphdata": [
 		{ 
 			"type": "work", 
-			"form": "Film", 
-			"theme": "Memory, Exile", 
-			"id": "Untitled2016", 
-			"label": "Untitled2016",
-			"details": "The starting point for this sketch is a deteriorated section of family footage from the EYE archive. The footage, barely distinguishable, traces the ephemeral outline of a woman and child paddling at the shore. <a href='http://research.sophie-dixon.com/immersive-media/untitled-2016/'>link to blog</a>",
-			//"vimeo": "162722775",
-			"image": "untitled2016.jpg",
-			"link": "http://research.sophie-dixon.com/immersive-media/untitled-2016/"
-		},
-		{ 
-			"type": "work", 
-			"form": "Text", 
-			"theme": "Memory", 
-			"id": "AndOnlyFineThreads", 
-			"label": "AndOnlyFineThreads", 
-			"details": "Text goes here"
-		},
-		{ 
-			"type": "work", 
-			"form": "Film", 
-			"theme": "Identity, TheSea", 
-			"id": "TheShore", 
-			"label": "The Shore", 
-			"details": "Text goes here"
-		},
-		{ 
-			"type": "work", 
-			"form": "Film", 
-			"theme": "Identity, Exile", 
-			"id": "ScholtzsHouse", 
-			"label": "Scholtz's House", 
-			"details": "Text goes here"
-		},
-		{ 
-			"type": "work", 
-			"form": "Film", 
-			"theme": "Identity, Mining", 
-			"id": "LaMortedlarbe", 
-			"label": "La Morte d'larbe", 
-			"details": "Text goes here" 
-		},
-		{ 
-			"type": "work", 
-			"form": "Text", 
-			"theme": "Storytelling", 
-			"id": "TheArtofNarrative", 
-			"label": "The Art of Narrative", 
-			"details": "Text goes here"
+			"form": "MovingImage", 
+			"theme": "Remembering, Place", 
+			"id": "BloodMoon", 
+			"label": "Blood Moon",
+			"details": "TA video examining the co-existence of places in memory layered onto physical space.",
+			"vimeo": "141019823",
+			//"image": "untitled2016.jpg",
+			"link": "http://sophie-dixon.com/project/blood-moon/"
 		},
 		{ 
 			"type": "theme", 
-			"id": "Memory", 
-			"label": "Memory", 
-			"details": "Text goes here"
+			"id": "Remembering", 
+			"label": "Remembering", 
+			"details": "Unlike the term memory, which may imply a fixed or static object. I use remembering to refer to an ongoing process in which the past is reformulated in the present."
 		},
 		{ 
 			"type": "theme", 
-			"id": "Identity", 
-			"label": "Identity", 
-			"details": "Text goes here"
+			"id": "Place", 
+			"label": "Place", 
+			"details": "Place description."
 		},
 		{ 
 			"type": "theme", 
-			"id": "Mining", 
-			"label": "Mining", 
-			"details": "Text goes here"
-		},
-		{ 
-			"type": "theme", 
-			"id": "Exile", 
-			"label": "Exile", 
-			"details": "Text goes here"
-		},
-		{ 
-			"type": "theme", 
-			"id": "Storytelling", 
-			"label": "Storytelling", 
-			"details": "Text goes here"
-		},
-		{ 
-			"type": "theme", 
-			"id": "TheSea", 
-			"label": "The Sea", 
-			"details": "Text goes here"
+			"id": "Story", 
+			"label": "Story", 
+			"details": "Story description."
 		},
 		{ 
 			"type": "form", 
-			"id": "Film", 
-			"label": "Film", 
-			"details": "Text goes here"
+			"id": "MovingImage", 
+			"label": "Moving Image", 
+			"details": "Moving image in all forms from desktop films to installation"
 		},
 		{ 
 			"type": "form", 
-			"id": "Text", 
-			"label": "Text", 
-			"details": "Text goes here"
+			"id": "Essay", 
+			"label": "Essay", 
+			"details": "Long-form critical reflections"
 		}
 	],
 	"details": {},
@@ -108,7 +50,7 @@ export default {
 		"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel interdum erat, sed pharetra dui. Sed vel odio ultricies, viverra est vitae, condimentum justo. Mauris sollicitudin, dolor quis aliquet posuere, ipsum nibh volutpat nulla, imperdiet aliquam lorem nisi id eros. Morbi quis ornare mi. Etiam quis eleifend justo, at facilisis sem. Nulla et pharetra neque. Curabitur a fermentum nisi. Suspendisse porttitor est luctus, fermentum quam bibendum, consectetur velit. Nullam nibh lacus, tempus nec maximus et, finibus sit amet orci."
 	},
 	"themedetails": { 
-		"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel interdum erat, sed pharetra dui. Sed vel odio ultricies, viverra est vitae, condimentum justo. Mauris sollicitudin, dolor quis aliquet posuere, ipsum nibh volutpat nulla, imperdiet aliquam lorem nisi id eros. Morbi quis ornare mi. Etiam quis eleifend justo, at facilisis sem. Nulla et pharetra neque. Curabitur a fermentum nisi. Suspendisse porttitor est luctus, fermentum quam bibendum, consectetur velit. Nullam nibh lacus, tempus nec maximus et, finibus sit amet orci."
+		"text": "The key themes of my research are Space and Narrative. Navigating this site using theme will enable you to see my research organised by its sub-themes themes. Namely remembering, storytelling, oral history, interactivity, place."
 	},
 	"aboutdetails": { 
 		"text": "<p>I am inspired by artists and filmmakers who experiment with the interstices and intersections between the viewer and their work. The question of how <i>space</i>, as physical distance, can generate new forms of narrative has been the foundation for my artistic research at the Netherlands Film Academy.</p><p><i><b>Spatial Narratives</b></i> ​ questions ways in which narratives can be experienced spatially by the viewer, but also the role of narrative in communicating and externalising the everyday spaces we inhabit. Whether geographical locations or virtual spaces in memory, it is my conjecture that a single linear narrative cannot adequately convey the multiplicity, fluidity and connectivity which define our experience of them. From traditional storytelling to Virtual Reality, I've stepped outside of the cinema to consider how space and narrative co-exist in other media and in other forms.</p><p><i>'Untitled'</i> combines the interactivity from Virtual Reality with the spatial and aesthetic qualities of a moving image installation. Through interaction with the installation the viewer experiences the co-existing narratives of two people, in two times, joined by a ruined house in the village of Srbská.</p><p><i>Srbská</i>, a place I lived in and returned to for almost a decade is known also as Wünschendorf; a thriving Sudeten village which witnessed the exile of its entire ethnic German population in 1946. Wünschendorf’s decline into the cluster of houses now clinging to the Czech and Polish border situate it in the narrative I find most fascinating of all, that of memory. I have travelled to Germany, the Czech Republic and Poland to record the testimonies of those who remember the village. With each narrative I encounter, the more certain I am that the village can only exist in a multiplicity of these various and often contradictory narratives.</p><p>This certainty is behind the creation of Srbska.org, an online archive and space for the contribution and collection of stories and artefacts from the village. Still in its infancy it continues to grow through collaboration with researchers, oral historians and archivists.</p><p>Finally, <i>Spatial Narratives</i> is a broad collection of essays, works and ideas designed to be navigated through either the form which my research has taken or the theme it has explored.</p>"
